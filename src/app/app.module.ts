7cc728a9214e132559ebf309ee8b6d9017d75663
PedroMDAndre <pedro.mda@gmail.com>
2021-05-04 11:19:56 +0100

    Creating Reusable Angular Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail-component';
 import { EventsListComponent } from './events/events-list.component'
+import { EventService } from './events/share/event.service';
 import { NavBarComponent } from './nav/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ToastrappService } from './common/toastrapp.service';
 
 @NgModule({
   imports: [
-    BrowserModule],
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService, ToastrappService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
