dd50864c1c7169cb1e0e75de130babb21fc52cb6
PedroMDAndre <pedro.mda@gmail.com>
2021-05-07 11:26:43 +0100

    Lazily Loading Feature Modules

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,9 +7,14 @@ import { EventRouteActivator } from "./events/event-details/event-route-activato
 import { EventListResolver } from "./events/events-list-resolver.service";
 
 export const appRoutes: Routes = [
-    { path: "events", component:EventsListComponent, resolve: {events: EventListResolver} },
-    { path: "events/new", component: CreateEventComponent, canDeactivate:['canDeactivateCreateEvent'] },
+    { path: "events", component: EventsListComponent, resolve: { events: EventListResolver } },
+    { path: "events/new", component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: "events/:id", component: EventDetailsComponent, canActivate: [EventRouteActivator] },
-    { path: "404", component: Error404Component},
-    { path: "", redirectTo: "/events", pathMatch: "full" }
+    { path: "404", component: Error404Component },
+    { path: "", redirectTo: "/events", pathMatch: "full" },
+    {
+        path: "user",
+        loadChildren: () => import("./user/user.module")
+            .then(m => m.UserModule)
+    }
 ];
