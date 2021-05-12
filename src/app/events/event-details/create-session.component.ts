c87b1c62238fb81c5f31495629dd8da94eb45d31
PedroMDAndre <pedro.mda@gmail.com>
2021-05-12 15:51:03 +0100

    passing data out of a child component

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ISession, restrictedWords } from "../shared";
 
 @Component({
+    selector: "create-session",
     templateUrl: "./create-session.component.html",
+
     styles: [`
      em {float:right; color:#E05C65; padding-left: 10px;}
   .error input, .error select, .error textarea {background-color:#E3C3C5;}
@@ -14,6 +16,9 @@ import { ISession, restrictedWords } from "../shared";
 `]
 })
 export class CreateSessionComponent implements OnInit {
+    @Output() saveNewSession = new EventEmitter();
+    @Output() cancelAddSession = new EventEmitter();
+
     newSessionForm!: FormGroup;
 
     name!: FormControl;
@@ -33,7 +38,8 @@ export class CreateSessionComponent implements OnInit {
                 Validators.required,
                 Validators.maxLength(400),
                 restrictedWords(["foo", "bar"])
-            ]);
+            ]
+        );
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -54,6 +60,11 @@ export class CreateSessionComponent implements OnInit {
             abstract: formValues.abstract,
             voters: []
         };
-        console.log(session);
+
+        this.saveNewSession.emit(session);
+    }
+
+    cancel() {
+        this.cancelAddSession.emit();
     }
 }
--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,22 +1,44 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "../shared/event.service";
 import { ActivatedRoute } from "@angular/router";
+import { ISession } from "../shared";
 
 @Component({
     templateUrl: "./event-details.component.html",
     styles: [`
     .container { padding-left: 20px; padding-right: 20px; }
-    .event-image { height: 100px; } `]
+    .event-image { height: 100px; }
+    a { cursor: pointer}
+     `]
 })
 export class EventDetailsComponent implements OnInit {
     event: any;
+    addMode: boolean = false;
 
     constructor(private eventService: EventService,
-                private route: ActivatedRoute){
+        private route: ActivatedRoute) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
     }
- }
+
+    addSession() {
+        this.addMode = true;
+    }
+
+    saveNewSession(session: ISession) {
+        const nextId = Math.max.apply(
+            null, this.event.sessions.map((s: any) => s.id)
+        );
+        session.id = nextId + 1;
+        this.event.sessions.push(session);
+        this.eventService.updateEvent(this.event);
+        this.addMode = false;
+    }
+
+    cancelAddSession() {
+        this.addMode = false;
+    }
+}
