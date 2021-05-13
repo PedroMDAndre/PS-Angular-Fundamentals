b3162d55f0e7b8b86a4f2aae1b60f3be040d2335
PedroMDAndre <pedro.mda@gmail.com>
2021-05-13 09:46:18 +0100

    Filtering data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -15,6 +15,8 @@ export class EventDetailsComponent implements OnInit {
     event: any;
     addMode: boolean = false;
 
+    filterBy: string = "all";
+
     constructor(private eventService: EventService,
         private route: ActivatedRoute) {
 
