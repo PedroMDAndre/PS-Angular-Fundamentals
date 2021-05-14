815adfc2e0f91ea4df389bb50af70d3dcb08d115
PedroMDAndre <pedro.mda@gmail.com>
2021-05-14 11:15:48 +0100

    Implementing the Session Search

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { EventService, ISession } from "../events";
 import { AuthService } from "../user/auth.service";
 
 @Component({
@@ -12,8 +13,21 @@ import { AuthService } from "../user/auth.service";
     `]
 })
 export class NavBarComponent {
-    constructor(public auth: AuthService) { 
+    searchTerm: string = "";
+    foundSessions!: ISession[];
 
+    constructor(public auth: AuthService,
+        private eventService: EventService) {
+
+    }
+
+    searchSessions(searchTerm: string) {
+        this.eventService.searchSession(searchTerm).subscribe(
+            sessions => {
+                this.foundSessions = sessions;
+                console.log(this.foundSessions);
+            }
+        );
     }
 
 }
