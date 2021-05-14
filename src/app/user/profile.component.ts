9e9ba428d118dfdf713cf7088bfa5ee5eff8888e
PedroMDAndre <pedro.mda@gmail.com>
2021-05-14 16:22:10 +0100

    profile component

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,60 +1,57 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from './auth.service';
-import { TOASTR_TOKEN, Toastr } from '../common/toastrapp.service';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit, Inject } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router'
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service';
 
 @Component({
-  templateUrl: "./profile.component.html",
+  templateUrl: './profile.component.html',
   styles: [`
-  em {float:right; color:#E05C65; padding-left: 10px;}
-  .error input {background-color:#E3C3C5;}
-  .error ::-webkit-input-placeholder { color: #999; }
-  .error ::-moz-placeholder { color: #999; }
-  .error :-moz-placeholder { color:#999; }
-  .error :ms-input-placeholder { color: #999; }
-`]
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color:#999; }
+    .error :ms-input-placeholder { color: #999; }
+  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm!: FormGroup;
   private firstName!: FormControl;
   private lastName!: FormControl;
 
-  constructor(
-    private auth: AuthService,
-    private router: Router,
-    private toastrString: ToastrService,
-    @Inject(TOASTR_TOKEN) private toastr: Toastr
-  ) { }
+  constructor(private router: Router, private authService: AuthService,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr) {
+
+  }
 
   ngOnInit() {
-    this.firstName = new FormControl(this.auth.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]);
-    this.lastName = new FormControl(this.auth.currentUser.lastName, Validators.required);
+    this.firstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')])
+    this.lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
 
     this.profileForm = new FormGroup({
       firstName: this.firstName,
       lastName: this.lastName
-    });
+    })
   }
 
   saveProfile(formValues: any) {
     if (this.profileForm.valid) {
-      this.auth.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.toastrString.success("Profile Saved");
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.toastr.success('Profile Saved')
     }
   }
 
-  cancel() {
-    this.router.navigate(["events"]);
-  }
-
-  validateFirstName(): boolean {
+  validateFirstName() {
     return this.firstName.valid || this.firstName.untouched
   }
 
-  validateLastName(): boolean {
+  validateLastName() {
     return this.lastName.valid || this.lastName.untouched
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
   }
 
 }
